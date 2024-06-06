6c04f87e92fd78a5e64547875fc34241ac172a0c
TheSnip <marctalla23@gmail.com>
2024-06-06 09:25:08 -0300

    1ere question

--- Examen-Final/todo/js/todo.js
+++ Examen-Final/todo/js/todo.js
@@ -1,3 +1,11 @@
-window.onload = function() {
-    document.getElementById('taskInput').focus();
-  };
+document.addEventListener('DOMContentLoaded', (e) => {
+
+
+    //Cette partie permet de mettre le curseur dans le champ InputTaks a l'ouverture de la page html
+    const inputTaks = document.getElementById('taskInput');
+    inputTaks.focus();
+
+   
+});
+
+    
